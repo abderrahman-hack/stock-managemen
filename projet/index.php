23cfbc39125cb7b6fb423812367060eddc25a2c5
abderrahman FENNANE <82466984+abderrahmanFennane@users.noreply.github.com>
2022-12-21 13:04:08 +0100

    Delete projet directory

--- projet/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-    header("location:pages/stagiaire.php")
-    
-?>
--- projet/pages/connectiondb.php
+++
@@ -1,7 +0,0 @@
-<?php
-    try{
-        $pdo=new PDO("mysql:host:=localhost;dbname=app","root","");
-    }catch(Exception $e){
-        die('Erreur de connexion : ' .$e->getMessage());
-    }
-?>
